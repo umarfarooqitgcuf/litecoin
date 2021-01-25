1370326a0ecd0c046a34bab380fb237c36104558
nexalt-project <60215685+nexalt-project@users.noreply.github.com>
2021-01-25 22:11:42 +0500

    difficulty issue

--- src/pow.cpp
+++ src/pow.cpp
@@ -36,6 +36,49 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
 {
 
     if ((pblock->GetBlockTime() >= START_POS_BLOCK) /*&& fProofOfStake*/) {
+        if (!fProofOfStake) {
+            pindexLast = GetLastBlockIndex(pindexLast, fProofOfStake);
+            //assert(pindexLast != nullptr);
+
+            unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
+            // Only change once per difficulty adjustment interval
+            if ((pindexLast->nHeight + 1) % params.DifficultyAdjustmentInterval() != 0) {
+                if (params.fPowAllowMinDifficultyBlocks) {
+                    // Special difficulty rule for testnet:
+                    // If the new block's timestamp is more than 2* 10 minutes
+                    // then allow mining of a min-difficulty block.
+                    if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing * 2) {
+                        return nProofOfWorkLimit;
+                    }
+                    else {
+                        // Return the last non-special-min-difficulty-rules-block
+                        const CBlockIndex *pindex = pindexLast;
+                        while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 &&
+                               pindex->nBits == nProofOfWorkLimit)
+                            pindex = pindex->pprev;
+                        return pindex->nBits;
+                    }
+                }
+                return pindexLast->nBits;
+            }
+
+            // Go back by what we want to be 14 days worth of blocks
+            // Nexalt: This fixes an issue where a 51% attack can change difficulty at will.
+            // Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
+            int blockstogoback = params.DifficultyAdjustmentInterval() - 1;
+            if ((pindexLast->nHeight + 1) != params.DifficultyAdjustmentInterval())
+                blockstogoback = params.DifficultyAdjustmentInterval();
+
+            // Go back by what we want to be 14 days worth of blocks
+            const CBlockIndex *pindexFirst = pindexLast;
+            for (int i = 0; pindexFirst && i < blockstogoback; i++)
+                pindexFirst = pindexFirst->pprev;
+
+            assert(pindexFirst);
+
+            return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
+        }
+        else {
         int64_t nTargetSpacing = params.nPowTargetSpacing;
         int64_t nTargetTimespan = params.nPowTargetTimespan;
         arith_uint256 bnTargetLimit = UintToArith256(params.powLimit).GetCompact();
@@ -76,7 +119,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
             bnNew = bnTargetLimit;
         }
         return bnNew.GetCompact();
-
+    }
     }else {
         if (pindexLast == nullptr){
             pindexLast = chainActive.Tip();
